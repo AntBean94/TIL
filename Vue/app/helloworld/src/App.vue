ee3df89f77b92c690610acafad615e6315933d74
gaudi.gwak <gaudi.gwak@kakaoenterprise.com>
2022-02-07 23:41:08 +0900

    TIL: Vue axios application

---
+++ Vue/app/helloworld/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    HelloWorld!
+    <axios></axios>
+  </div>
+</template>
+
+<script>
+
+import axios from '@/components/axios'
+
+export default {
+  name: 'App',
+  components: {
+    axios
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue/app/helloworld/src/components/axios.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <button v-if="!result" @click="fetchData">
+        clickme!
+    </button>
+    <div v-else>
+        {{ result }}
+        <br>
+        <button @click="initializer">
+            back
+        </button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            result: ""
+        }
+    },
+    methods: {
+        fetchData: function() {
+            axios.get('https://jsonplaceholder.typicode.com/users/')
+            .then((res) => {
+                console.log(res)
+                this.result = res.data[0]["name"]
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        },
+        initializer: function() {
+            this.result = ""
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
