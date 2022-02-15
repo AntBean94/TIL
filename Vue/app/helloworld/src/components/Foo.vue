3aaa7dd834dc770855f38c6aadcb84b69c873a5d
gaudi.gwak <gaudi.gwak@kakaoenterprise.com>
2022-02-16 00:14:09 +0900

    TIL: Vue v-slot

--- Vue/app/helloworld/src/components/Foo.vue
+++ Vue/app/helloworld/src/components/Foo.vue
@@ -1,10 +1,28 @@
 <template>
-  
+  <div>
+      <!-- 헤더 영역 -->
+      <slot name="header"></slot>
+      <!-- 본문 영역 -->
+      <slot></slot>
+      <br>
+      <br>
+      <!-- scoped slot -->
+      <slot name="scoped" :msg="msg"></slot>
+      <br>
+      <slot name="special" :msg="msg2"></slot>
+      <!-- 푸터 영역 -->
+      <slot name="footer"></slot>
+  </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            msg: "지금 보시는 이 문구는 자식 컴포넌트에 정의되어있습니다. scoped-slot은 자식컴포넌트에 정의되어있는 데이터를 부모 컴포넌트에서 보여줄 수 있도록 합니다.",
+            msg2: "hi there"
+        }
+    }
 }
 </script>
 
--- Vue/app/helloworld/src/components/SlotContainer.vue
+++ Vue/app/helloworld/src/components/SlotContainer.vue
@@ -1,7 +1,32 @@
 <template>
     <div>
         <h2>{{ char }}</h2>
-        <Foo></Foo>        
+        <Foo>
+            <template v-slot:header>
+                <h1>v-slot</h1>
+            </template>
+
+            <!-- default는 이름을 붙이지 않은 slot에 표현된다. -->
+            <template v-slot:default>
+                <b>
+                    슬롯은 자식 컴포넌트를 부모컴포넌트에서 사용하고자 하는 시점에 내부를 정의할 수 있다는 점에서 유용하다.
+                    이는 컴포넌트의 재사용성과도 밀접한 관계가 있다. 
+                </b>
+            </template>
+
+            <!-- scoped slot -->
+            <template v-slot:scoped="slotProps">
+                <b>{{ slotProps.msg }}</b>
+            </template>
+            <!-- 아래와 같이 특수기호 #을 사용해서 v-slot을 표현할 수 있다. -->
+            <template #special="slotProps">
+                <b>{{ slotProps.msg }}</b>
+            </template>
+
+            <template v-slot:footer>
+                <h5>Footer</h5>
+            </template>
+        </Foo>        
     </div>
 </template>
 
