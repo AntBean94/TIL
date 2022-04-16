ff5c6ebde09cc3cc3954880e240f00c9d384131f
gwak-ongyeom <dhsrua2015@gmail.com>
2022-04-16 19:56:51 +0900

    java tutorial 다차원 배열

---
+++ Java/Tutorial13/Main.java
@@ -0,0 +1,24 @@
+package Java.Tutorial13;
+
+public class Main {
+    // 다차원 배열
+
+    public static void main(String[] args) {
+
+        int N = 50;
+        // 다차원 배열 선언
+        int[][] array = new int[N][N];
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                array[i][j] = (int)(Math.random() * 10);
+            }
+        }
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
