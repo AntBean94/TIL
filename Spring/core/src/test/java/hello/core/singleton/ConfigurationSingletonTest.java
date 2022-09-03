4b4be8474d12e44ecd79889f9269b5f9da2aca31
gwak-ongyeom <dhsrua2015@gmail.com>
2022-09-03 13:54:07 +0900

    스프링 핵심원리 - @Configuration 과 바이트코드 조작으로 싱글톤 구현

--- Spring/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ Spring/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -33,4 +33,20 @@ public class ConfigurationSingletonTest {
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
 
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // AppConfig도 빈으로 등록된다.
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        // bean = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$a6b188b8 출력
+        System.out.println("bean = " + bean.getClass());
+
+        // AppConfig가 아니라 CGLIB가 붙은 복잡한 클래스가 출력되는것을 확인할 수 있다.
+        // @Configuration 어노테이션이 붙으면 바이트코드를 조작해 AppConfig를 상속하여 싱글톤을 보장하는 클래스를 만든다.
+        // @Configuration 어노테이션이 없으면 memberRepository도 각각 다른 인스턴스가 출력된다.
+
+    }
 }
