613166cb3f4f1896572ddf1e903138c95f21a1ad
gaudi.gwak <gaudi.gwak@kakaoenterprise.com>
2023-03-26 17:45:02 +0900

    HTTP 요청 데이터 - API 메시지 바디 - 단순 텍스트

--- Spring/servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ Spring/servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -25,6 +25,7 @@ public class RequestParamServlet extends HttpServlet {
         String[] usernames = request.getParameterValues("username"); //복수 파라미터 조회
      */
 
+    @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("[전체 파라미터 조회] - start");
 
