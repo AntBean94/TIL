bb9a21f0901decf15be6b924b83ad8a0a5f0638c
gaudi.gwak <gaudi.gwak@kakaoenterprise.com>
2023-08-15 17:54:56 +0900

    스프링mvc - redirectAttributes

--- Spring/item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ Spring/item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -80,10 +81,18 @@ public class BasicItemController {
     /**
      * PRG - Post/Redirect/Get
      */
+//    @PostMapping("/add")
+//    public String addItemV5(Item item) {
+//        itemRepository.save(item);
+//        return "redirect:/basic/items/" + item.getId();
+//    }
+
     @PostMapping("/add")
-    public String addItemV5(Item item) {
-        itemRepository.save(item);
-        return "redirect:/basic/items/" + item.getId();
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
@@ -98,7 +107,6 @@ public class BasicItemController {
         itemRepository.update(itemId, item);
         return "redirect:/basic/items/{itemId}";
     }
-
 
     /*
     * 테스트 데이터 추가
