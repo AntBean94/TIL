72abee1101c2908e307a7ec4859d73fdb0747b19
ongyeom-gwak <dhsrua2015@gmail.com>
2023-10-11 22:30:33 +0900

    spring jpa - 상품 수정 컨트롤러, 템플

--- Spring/jpashop/src/main/java/jpabook/jpashop/web/ItemController.java
+++ Spring/jpashop/src/main/java/jpabook/jpashop/web/ItemController.java
@@ -1,12 +1,17 @@
 package jpabook.jpashop.web;
 
 import jpabook.jpashop.domain.items.Book;
+import jpabook.jpashop.domain.items.Item;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,6 +29,57 @@ public class ItemController {
     @PostMapping(value = "/items/new")
     public String create(BookForm form) {
         Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
+
+    @GetMapping(value = "/items")
+    public String list(Model model) {
+
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+
+    /**
+     * 상품 수정 폼
+     * @param itemId
+     * @param model
+     * @return
+     */
+    @GetMapping(value = "/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+
+        Book book = (Book) itemService.findOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(book.getId());
+        form.setName(book.getName());
+        form.setPrice(book.getPrice());
+        form.setStockQuantity(book.getStockQuantity());
+        form.setAuthor(book.getAuthor());
+        form.setIsbn(book.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    /**
+     * 상품 수정
+     * @param form
+     * @return
+     */
+    @PostMapping(value = "items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("form") BookForm form) {
+
+        Book book = new Book();
+        book.setId(form.getId());
         book.setName(form.getName());
         book.setPrice(form.getPrice());
         book.setStockQuantity(form.getStockQuantity());
