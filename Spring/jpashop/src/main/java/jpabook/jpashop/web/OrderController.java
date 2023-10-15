ca32cb76aea9e8b8ca8ac3de9074a76113d16fe3
ongyeom-gwak <dhsrua2015@gmail.com>
2023-10-15 20:24:19 +0900

    spring jpa - 주문 목록 컨트롤러, 템플릿

--- Spring/jpashop/src/main/java/jpabook/jpashop/web/OrderController.java
+++ Spring/jpashop/src/main/java/jpabook/jpashop/web/OrderController.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.web;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderSearch;
 import jpabook.jpashop.domain.items.Item;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.service.ItemService;
@@ -9,10 +11,7 @@ import jpabook.jpashop.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -44,6 +43,14 @@ public class OrderController {
         return "redirect:/orders";
     }
 
+    @GetMapping(value = "/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+
+        return "order/orderList";
+    }
 
 
 }
