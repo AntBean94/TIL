576fc9cde290809abbe0ddf48309650ead976f8c
ongyeom-gwak <dhsrua2015@gmail.com>
2023-10-16 23:19:43 +0900

    스프링 mvc - 타임리프 기본기능(escape)

--- Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -15,5 +15,17 @@ public class BasicController {
         return "basic/text-basic";
     }
 
+    /**
+     * 타임리프가 제공하는 th:text, [[...]] 표현식은 기본적으로
+     * 이스케이프(escape)를 제공한다.
+     * 따라서, 이스케이프를 적용하지 않으려면 별도의 표현식을 사용해야한다.
+     * => th:utext, [(...)]
+     */
+    @GetMapping(value = "text-unescaped")
+    public String textUnescaped(Model model) {
+        model.addAttribute("data", "Hello <b>Spring!</b>");
+        return "basic/text-unescaped.html";
+    }
+
 
 }
