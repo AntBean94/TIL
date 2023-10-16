2b3b3ae699a046a3e0c818c5902af0f9b7c6883b
ongyeom-gwak <dhsrua2015@gmail.com>
2023-10-16 23:28:48 +0900

    스프링 mvc - 타임리프 기본기능(변수, SpringEL 표현식)

--- Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,15 +1,21 @@
 package hello.thymeleaf.basic;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/basic")
 public class BasicController {
 
-    @GetMapping(value = "text-basic")
+    @GetMapping(value = "/text-basic")
     public String textBasic(Model model) {
         model.addAttribute("data", "Hello Spring");
         return "basic/text-basic";
@@ -21,10 +27,44 @@ public class BasicController {
      * 따라서, 이스케이프를 적용하지 않으려면 별도의 표현식을 사용해야한다.
      * => th:utext, [(...)]
      */
-    @GetMapping(value = "text-unescaped")
+    @GetMapping(value = "/text-unescaped")
     public String textUnescaped(Model model) {
         model.addAttribute("data", "Hello <b>Spring!</b>");
         return "basic/text-unescaped.html";
+    }
+
+    @GetMapping(value = "/variable")
+    public String variable(Model model) {
+
+        User userA = new User("userA", 15);
+        User userB = new User("userB", 20);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("list", list);
+        model.addAttribute("map", map);
+
+        return "basic/variable";
+
+    }
+
+    @Data
+    static class User {
+
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
     }
 
 
