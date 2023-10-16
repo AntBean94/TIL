cf35a6f69634bb40e087888c91601d40faa11b14
ongyeom-gwak <dhsrua2015@gmail.com>
2023-10-16 23:50:53 +0900

    스프링 mvc - 타임리프 기본기능(기본객체)

--- Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,11 +1,16 @@
 package hello.thymeleaf.basic;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -52,8 +57,40 @@ public class BasicController {
         model.addAttribute("map", map);
 
         return "basic/variable";
-
     }
+
+    /**
+     * test url : http://localhost:8080/basic/basic-objects?paramData=HelloParam
+     */
+    @GetMapping(value = "/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello" + data;
+        }
+    }
+
+    /**
+     * 스프링부트 3.0 이상 버전이라면 다음과 같이 사용한다.
+     * - 기본객체(request, response, session, servletContext)를 지원하지 않음.
+     */
+    @GetMapping("/basic-objects/V3")
+    public String basicObjectsV3(Model model,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response,
+                                 HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session!");
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+        return "basic/basic-objects";
+    }
+
 
     @Data
     static class User {
