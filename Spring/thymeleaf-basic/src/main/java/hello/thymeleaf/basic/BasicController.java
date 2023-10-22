77b58177e2bab96540dde4104c553cc6ea8a2b06
ongyeom-gwak <dhsrua2015@gmail.com>
2023-10-22 21:41:24 +0900

    스프링 mvc - 타임리프기본기능(자바스크립트 인라인)

--- Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -150,6 +150,15 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+
+        model.addAttribute("user", new User("userA", 10));
+        addUser(model);
+
+        return "basic/javascript";
+    }
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
