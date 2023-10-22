3fcdf8226cf69a2cf92f3b9ea8f8167fb677afe3
ongyeom-gwak <dhsrua2015@gmail.com>
2023-10-22 21:29:32 +0900

    스프링 mvc - 타임리프기본기능(블록)

--- Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -144,6 +144,12 @@ public class BasicController {
         return "basic/comments";
     }
 
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUser(model);
+        return "basic/block";
+    }
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
